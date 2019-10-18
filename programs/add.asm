3cab947ae749d34270a63253ef011435bb69e6d4
Michael B <michael@ramyun.co.uk>
2019-10-18 21:27:49 +0100

    Add loadstore.asm

--- programs/add.asm
+++ programs/add.asm
@@ -1,4 +1,4 @@
-move r2 5    # r2 <- 5
-move r3 6    # r3 <- 5
+move r1 5    # r2 <- 5
+move r2 6    # r3 <- 5
 
-add r1 r2 r3 # r1 <- r2 + r3
+add r0 r1 r2 # r1 <- r2 + r3
---
+++ programs/loadstore.asm
@@ -0,0 +1,6 @@
+move r1 5    # r2 <- 5
+move r2 6    # r3 <- 5
+
+sai r1 0     # [0] <- r1
+lai r3 0     # r3 <- [0]
+add r0 r3 r2 # r0 <- r3 + r2
