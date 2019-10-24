1da7bcb0eb16c4f32ab8cfbcaec61746b3e2de1c
Michael B <michael@ramyun.co.uk>
2019-10-24 18:14:12 +0100

    Add initial branch and jump opcodes

--- src/instruction/Opcode.java
+++ src/instruction/Opcode.java
@@ -13,12 +13,16 @@ public enum Opcode {
   MOVE(OpcodeCategory.ARITHMETIC, 2), /* move r1 v1    = r1 <- v1 */
 
   /* Memory */
-  LA(OpcodeCategory.MEMORY, 3),   /* la   r1 r2 v1 = r1 <- [r2 + v1] */
-  LAI(OpcodeCategory.MEMORY, 2),  /* lai  r1 v1    = r1 <- [v1] */
-  SA(OpcodeCategory.MEMORY, 3),   /* sa   r1 r2 v1 = [r2 + v1] <- r1 */
-  SAI(OpcodeCategory.MEMORY, 2),  /* sai  r1 v1    = [v1] <- r1 */
+  LA(OpcodeCategory.MEMORY, 3),       /* la   r1 r2 v1 = r1 <- [r2 + v1] */
+  LAI(OpcodeCategory.MEMORY, 2),      /* lai  r1 v1    = r1 <- [v1] */
+  SA(OpcodeCategory.MEMORY, 3),       /* sa   r1 r2 v1 = [r2 + v1] <- r1 */
+  SAI(OpcodeCategory.MEMORY, 2),      /* sai  r1 v1    = [v1] <- r1 */
 
-  /* ... */
+  /* Control */
+  BEQ(OpcodeCategory.CONTROL, 3),     /* beq  r1 r2 v1 = PC -= {v1 if r1 = r2, 0 if r1 != r2} */
+  BNE(OpcodeCategory.CONTROL, 3),     /* bne  r1 r2 v1 = PC -= {0 if r1 = r2, v1 if r1 != r2} */
+  JMP(OpcodeCategory.CONTROL, 1),     /* jmp v1        = PC <- v1 */
+  JMPR(OpcodeCategory.CONTROL, 1),    /* jmpr r1       = PC <- r1 */
   ;
 
   private final String instructionPrefix;
