688ef542f3c40d3ce6372ffdf62ef3bdcbe6edfb
Michael B <michael@ramyun.co.uk>
2019-10-28 18:28:25 +0000

    Updated factorial.asm

--- programs/factorial.asm
+++ programs/factorial.asm
@@ -1,7 +1,7 @@
-move r0 1       # 0 | r0 <- 1 (Counter variable)
+move r0 12      # 0 | r0 <- 12 (Factorial to calculate)
 move r1 1       # 1 | r1 <- 1 (Running product)
-move r2 12      # 2 | r2 <- 12 (Factorial to calculate)
-bgt r0 r2 3     # 3 | PC += {3 if r0 > r2, 0 otherwise}
-mul r1 r1 r0    # 4 | r1 <- r1 * r0
-addi r0 r0 1    # 5 | r0 <- r0 + 1
+move r2 1       # 2 | r2 <- 1 (Counter variable)
+bgt r2 r0 3     # 3 | PC += {3 if r2 > r0, 0 otherwise}
+mul r1 r1 r2    # 4 | r1 <- r1 * r2
+addi r2 r2 1    # 5 | r2 <- r2 + 1
 jmp 3           # 6 | PC <- 3
