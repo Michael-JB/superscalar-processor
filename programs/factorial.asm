785b33adb7d0b34fa98d0300cedad8bb729870c9
Michael B <michael@ramyun.co.uk>
2019-12-08 18:10:51 +0000

    Add labels to programs

--- programs/factorial.asm
+++ programs/factorial.asm
@@ -1,8 +1,10 @@
-move r0 12      # 0 | r0 <- 12 (Factorial to calculate)
-move r1 1       # 1 | r1 <- 1 (Running product)
-move r2 1       # 2 | r2 <- 1 (Counter variable)
-bgt r2 r0 4     # 3 | PC += {4 if r2 > r0, 0 otherwise}
-mul r1 r1 r2    # 4 | r1 <- r1 * r2
-addi r2 r2 1    # 5 | r2 <- r2 + 1
-jmp 3           # 6 | PC <- 3
-nop             # 7 |
+init: move r0 12      # r0 <- 12 (Factorial to calculate)
+      move r1 1       # Init running product
+      move r2 1       # Init counter
+
+loop: bgt r2 r0 end   # Go to end if counter exceeds factorial to calculate
+      mul r1 r1 r2    # Perform multiplication
+      addi r2 r2 1    # Increment counter variable
+      jmp loop
+
+ end: nop
